1ff7a62b1c24bf94881f34c34a5246523267a420
Josleke <josleke@gmail.com>
2026-02-07 19:07:12 +0100

    Production update

--- src/screens/Auth/LoginRedirect/index.tsx
+++ src/screens/Auth/LoginRedirect/index.tsx
@@ -17,11 +17,8 @@ export const LoginRedirect = () => {
             try {
                 const authUrl = await getAuthorizerUrl();
                 const url = authUrl?.data;
-                alert('Please login to continue');
-                alert(url);
+
                 if (url) {
-                    alert('Please login to continue');
-                    alert(url);
                     // @ts-ignore
                     window.location.replace(url);
                 }
